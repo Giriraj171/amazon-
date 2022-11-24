9af8afe0f1c0fa4f9b852d1b3d2899aa9ca54d35
giriraj171 <giriraj171@gmail.com>
2022-11-24 18:08:11 +0530

    i need 30

---
+++ login.java
@@ -0,0 +1,12 @@
+public class login
+{
+        public static void main(string args[])
+        {
+                int a = 30;
+
+                for (i=0;i<=10;i++)
+                {
+                        system.out.println ("Number: " +a);
+                }
+        }
+}
